832ec2a525958a89e6d4caa3cf2b1a546e9512d8
dehmer <horst.dehmer@syncpoint.io>
2024-12-20 08:31:19 +0100

    transduce.

--- test/Monad.test.js
+++ test/Monad.test.js
@@ -2,10 +2,6 @@ import assert from 'node:assert'
 import * as R from 'ramda'
 import { describe, it } from "vitest"
 
-const AsyncIterator = function (fn) {
-  this[Symbol.asyncIterator] = fn
-}
-
 const wrapArray = xs => async function* () {
   for (const x of xs) yield x
 }
@@ -35,6 +31,10 @@ const reduce = R.curry(async (fn, init, it) => {
   for await (const i of it) acc = fn(acc, i)
   return acc
 })
+
+const AsyncIterator = function (fn) {
+  this[Symbol.asyncIterator] = fn
+}
 
 AsyncIterator.of =
 AsyncIterator['fantasy-land/of'] =
@@ -106,35 +106,98 @@ describe('AsyncIterator', () => {
 
   it('AsyncIterator.of [Symbol.iterator]', async () => {
     const expected = [0, 12, 31]
-    const it = AsyncIterator.of(syncIterator(expected))
+    const it = AsyncIterator.of(asyncIterator(expected))
     const actual = await Array.fromAsync(it)
     assert.deepStrictEqual(actual, expected)
   })
 
-  it('map', async () => {
+  it('[built-in] map', async () => {
     const double = x => x * 2
     const range = R.range(0, 10)
-    const it = AsyncIterator.of(syncIterator(range))
+    const it = AsyncIterator.of(asyncIterator(range))
     const expected = range.map(double)
     const actual = await Array.fromAsync(it.map(double))
     assert.deepStrictEqual(actual, expected)
   })
 
-  it('filter', async () => {
+  it('[built-in] filter', async () => {
     const odd = x => x % 2
     const range = R.range(0, 10)
-    const it = AsyncIterator.of(syncIterator(range))
+    const it = AsyncIterator.of(asyncIterator(range))
     const expected = range.filter(odd)
     const actual = await Array.fromAsync(it.filter(odd))
     assert.deepStrictEqual(actual, expected)
   })
 
-  it('chain', async () => {
+  it('[built-in] chain', async () => {
     const fill = x => AsyncIterator.of(Array(x).fill(x))
     const range = R.range(0, 10)
-    const it = AsyncIterator.of(syncIterator(range))
+    const it = AsyncIterator.of(asyncIterator(range))
     const expected = R.chain(x => Array(x).fill(x), range)
     const actual = await Array.fromAsync(it.chain(fill))
     assert.deepStrictEqual(actual, expected)
+  })
+
+  it('[Ramda] map', async () => {
+    const double = x => x * 2
+    const range = R.range(0, 10)
+    const it = AsyncIterator.of(asyncIterator(range))
+    const expected = range.map(double)
+    const actual = await Array.fromAsync(R.map(double, it))
+    assert.deepStrictEqual(actual, expected)
+  })
+
+  it('[Ramda] map (multiple)', async () => {
+    const range = R.range(0, 10)
+    const f = R.compose(R.map(R.add(2)), R.map(R.multiply(3)), R.map(R.add(1)))
+    const it = AsyncIterator.of(asyncIterator(range))
+    const actual = await Array.fromAsync((f(it)))
+    assert.deepStrictEqual(actual, [5, 8, 11, 14, 17, 20, 23, 26, 29, 32])
+  })
+
+  it('[Ramda] filter', async () => {
+    const odd = x => x % 2
+    const range = R.range(0, 10)
+    const it = AsyncIterator.of(asyncIterator(range))
+    const expected = range.filter(odd)
+    const actual = await Array.fromAsync(R.filter(odd, it))
+    assert.deepStrictEqual(actual, expected)
+  })
+
+  it('[Ramda] chain', async () => {
+    const fill = x => AsyncIterator.of(Array(x).fill(x))
+    const range = R.range(0, 10)
+    const it = AsyncIterator.of(asyncIterator(range))
+    const expected = R.chain(x => Array(x).fill(x), range)
+    const actual = await Array.fromAsync(R.chain(fill, it))
+    assert.deepStrictEqual(actual, expected)
+  })
+
+  const asyncTransformer = fn => ({
+    '@@transducer/step': (acc, x) =>
+      acc['@@transducer/reduced']
+        ? acc['@@transducer/value']
+        : fn(acc, x)
+        ,
+    '@@transducer/result': async acc =>
+      (await acc)['@@transducer/reduced']
+        ? (await acc)['@@transducer/value']
+        : x
+  })
+
+  it('[Ramda] transduce', async () => {
+    const range = R.range(0, 10)
+    const it = AsyncIterator.of(asyncIterator(range))
+
+    const fn = R.compose(
+      R.map(R.multiply(3)),
+      R.filter(x => x % 2),
+      R.map(R.add(4)),
+      R.take(3)
+    )
+
+    const xf = asyncTransformer(R.flip(R.append))
+    const actual = await R.transduce(fn, xf, [], it)
+    assert.deepStrictEqual(actual, [7, 13, 19])
   })
 })
